b7142e080da2b3aeca06e77215f00dd7578ac8eb
PlamenHP <p.petrov.mail@gmail.com>
2016-02-27 10:17:18 +0000

    Adding test console application

---
+++ ConsoleTest/ConsoleTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TFSTest
+{
+    class TFSTest
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, TFS");
+            Console.WriteLine("TFS Problems");
+        }
+    }
+}
