c68b2f923dc7ebed976f20c7395e3ab99aa7fa86
PlamenHP <p.petrov.mail@gmail.com>
2016-02-28 20:06:00 +0000

    Added Telerik Academy: C# Part 1, Exam 3 Enigmation

---
+++ Exam_CSharp_Part_1/Enigmanation/Enigmanation/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Enigmanation
+{
+    class Enigmanation
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+
+            decimal result = 0;
+            decimal bracketResult = 0;
+            char currentOperator = '+';
+            char bracketOperator = '+';
+            bool inBracket = false;
+            foreach (var symbol in expression)
+            {
+                if (inBracket)
+	            {
+                    if (symbol == '+' ||
+                        symbol == '-' ||
+                        symbol == '*' ||
+                        symbol == '%')
+                    {
+                        bracketOperator = symbol;
+                    }
+
+                    if (symbol >= '0' && symbol <= '9')
+                    {
+                        int currentNumber = symbol - '0';
+
+                        switch (bracketOperator)
+                        {
+                            case '+': bracketResult += currentNumber; break;
+                            case '-': bracketResult -= currentNumber; break;
+                            case '*': bracketResult *= currentNumber; break;
+                            case '%': bracketResult %= currentNumber; break;
+                        }
+                    }		 
+	            }
+                else
+                {
+                    if (symbol == '+' ||
+                        symbol == '-' ||
+                        symbol == '*' ||
+                        symbol == '%')
+                    {
+                        currentOperator = symbol;
+                    }
+
+                    if (symbol >= '0' && symbol <= '9')
+                    {
+                        int currentNumber = symbol - '0';
+
+                        switch (currentOperator)
+                        {
+                            case '+': result += currentNumber; break;
+                            case '-': result -= currentNumber; break;
+                            case '*': result *= currentNumber; break;
+                            case '%': result %= currentNumber; break;
+                        }
+                    }
+                }
+
+                if (symbol == '(')
+                {
+                    inBracket = true;
+                    continue;
+                }
+
+                if (symbol == ')')
+                {
+                    switch (currentOperator)
+                    {
+                        case '+': result += bracketResult; break;
+                        case '-': result -= bracketResult; break;
+                        case '*': result *= bracketResult; break;
+                        case '%': result %= bracketResult; break;
+                    }
+
+                    bracketResult = 0;
+                    bracketOperator = '+';
+                    inBracket = false;
+                }
+            }
+
+            Console.WriteLine("{0:F3}", result);
+        }
+
+    }
+}
---
+++ Exam_CSharp_Part_1/Nightmare on Code Street/Nightmare on Code Street/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Nightmare_on_Code_Street
+{
+    class Nightmare_on_Code_Street
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int totalOddNumbers = 0;
+            int sum = 0;
+            for (int i = 1; i < input.Length; i+=2)
+            {
+                if ((input[i] >= '0') && (input[i] <= '9'))
+                {
+                    totalOddNumbers++;
+                    sum += (input[i] - '0');
+                }
+            }
+
+            Console.Write(totalOddNumbers);
+            Console.Write(" ");
+            Console.Write(sum);
+        }
+    }
+}
