cd1699f409317eced5e899795a14f1a4b4da7b78
PlamenHP <p.petrov.mail@gmail.com>
2016-02-29 18:54:15 +0000

    Telerik Academy, CSharp Part 1, Exam Added: Diamond Trolls

---
+++ Exam_CSharp_Part_1/Diamond Trolls/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Diamond_Trolls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int height = (6 + ((n - 3) / 2) * 3);
+            int width = (2*n)+1; 
+
+            //diamond top
+            Console.WriteLine(new string('.',(width - n)/2) + 
+                              new string('*',n) + 
+                              new string('.',(width - n)/2));
+
+            //diamond upper part
+            for (int i = 0; i < (n-1)/2; i++)
+            {
+                Console.WriteLine(new string('.', ((n - 1) / 2) - i) + 
+                                  "*" +
+                                  new string('.', ((n - 1) / 2) + i) +
+                                  "*" +
+                                  new string('.', ((n - 1) / 2) + i) +
+                                  "*" +
+                                  new string('.', ((n - 1) / 2) - i));
+            } 
+
+
+            //diamond middle
+            Console.WriteLine(new string('*', width));
+
+            //diamond lower part
+            for (int i = 0; i < n-1 ; i++)
+            {
+                Console.WriteLine(new string('.', (1 + i)) +
+                                  "*" +
+                                  new string('.', (n - 2) - i) +
+                                  "*" +
+                                  new string('.', (n - 2) - i) +
+                                  "*" +
+                                  new string('.', (1 + i)));
+            }
+
+            //diamond bottom
+            Console.WriteLine(new string('.', n) + "*" + new string('.', n));
+        }
+    }
+}
