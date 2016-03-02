14431316853cd921d006ec73d3ae121b2d82031f
PlamenHP <p.petrov.mail@gmail.com>
2016-03-02 23:50:52 +0000

    Telerik Academy SCharp Part 1 Exam: Added: Problem 1 Printing

---
+++ Exam_CSharp_Part_1/Problem 1 – Printing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Problem_1_Printing
+{
+    class Problem_1_Printing
+    {
+        public static void Main()
+        {
+            int nStudentCount = 0;
+            int sSheetsPerStudent = 0;
+            decimal pRealmPrice = 0;
+            const int SHEETS_IN_REALM = 500;
+
+            nStudentCount = int.Parse(Console.ReadLine());
+            sSheetsPerStudent = int.Parse(Console.ReadLine());
+            pRealmPrice = decimal.Parse(Console.ReadLine());
+
+            int totalSheetsOfPaper = nStudentCount * sSheetsPerStudent;
+            decimal realmsCount = (decimal)totalSheetsOfPaper / SHEETS_IN_REALM;
+            decimal totalCost = realmsCount * pRealmPrice;
+
+            Console.WriteLine("{0:F2}", totalCost);
+        }
+    }
+}
