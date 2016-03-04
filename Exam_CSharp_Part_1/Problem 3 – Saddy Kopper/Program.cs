eaa8044371522bba8aa7a4d4ec153c6ba11ace5c
PlamenHP <p.petrov.mail@gmail.com>
2016-03-04 02:07:06 +0000

    Telerik Academy C# Part 1 Exam Added: Problem 3 Saddy Kopper

---
+++ Exam_CSharp_Part_1/Problem 3 – Saddy Kopper/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Saddy_Kopper
+{
+    public class Saddy_Kopper
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            short transformations = 0;
+            List<int> oddNumberSumList = new List<int>();
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < input.Length; i++)
+			{
+			    numbers.Add(input[i] - '0');
+			}
+
+            while (numbers.Count > 1)
+            {
+                while (numbers.Count > 1)
+                {
+                    numbers.RemoveAt(numbers.Count-1);
+                    int oddNumbersSum = 0;
+                    for (int i = 0; i < numbers.Count; i++)
+			        {
+                        if (((i - '0') % 2) == 0)
+                        {
+                            oddNumbersSum += numbers[i];
+                        }	 
+			        }
+                    oddNumberSumList.Add(oddNumbersSum);
+                }
+
+                BigInteger sumProduct = 1;
+                foreach (var sum in oddNumberSumList)
+                {
+                    sumProduct *= sum;
+                }
+                oddNumberSumList.Clear();
+                string newNumbers = sumProduct.ToString();
+                numbers.Clear();
+                for (int i = 0; i < newNumbers.Length; i++)
+                {
+                    numbers.Add(newNumbers[i] - '0');
+                }
+                transformations++;
+
+                if (transformations == 10)
+                {
+                    break;
+                }
+            }
+
+            if (transformations == 10)
+            {
+                string output= string.Join("",numbers);
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine(transformations.ToString());
+                Console.WriteLine(numbers[0].ToString());
+            }
+        }
+    }
+}
