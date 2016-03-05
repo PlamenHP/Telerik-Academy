13d5338de7f5b9febe2405858b00dc3deec0f592
PlamenHP <p.petrov.mail@gmail.com>
2016-03-05 11:30:49 +0000

    Telerik academy C# part 1 Exam added: Problem 4 Cube

---
+++ Exam_CSharp_Part_1/Problem 4 - Cube/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Cube
+{
+    public class Cube
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(new string(' ', n - 1) + new string(':', n));
+
+            for (int i = 1; i <= (n - 2); i++)
+            {
+                Console.WriteLine(new string(' ', (n - i - 1)) + new string(':', 1) + new string('/', (n - 2)) + new string(':', 1) + new string('X', (i - 1)) + new string(':', 1));
+            }
+
+            Console.WriteLine(new string(':', n) + new string('X', n - 2) + new string(':', 1));
+
+            for (int i = 1; i <= (n - 2); i++)
+            {
+                Console.WriteLine(new string(':',1) + new string(' ', (n-2)) + new string(':', 1) + new string('X', n-i -2) + new string(':', 1));
+            }
+
+            Console.WriteLine(new string(':', n) + new string(' ', n - 1));
+        }
+    }
+}
