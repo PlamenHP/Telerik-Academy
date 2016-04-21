794d1bb280cd957c02f77b53cbc170c9248c1e53
PlamenHP <p.petrov.mail@gmail.com>
2016-04-21 01:37:35 +0100

    tasks

--- C#2/Homework/Multidimensional-Arrays/SequenceNMatrix/SequenceNMatrix.cs
+++ C#2/Homework/Multidimensional-Arrays/SequenceNMatrix/SequenceNMatrix.cs
@@ -22,22 +22,26 @@ namespace Namespace
         {
             Console.WriteLine("Problem 3. Sequence n matrix \n");
             Console.Write("enter rows count (N>=3) N= ");
-            int rows = int.Parse(Console.ReadLine());
+            //int rows = int.Parse(Console.ReadLine());
             Console.Write("enter columns count (M>=3) M= ");
-            int columns = int.Parse(Console.ReadLine());
-            string[,] matrix = new string[rows, columns];
+            //int columns = int.Parse(Console.ReadLine());
+            //string[,] matrix = new string[rows, columns];
+            string[,] matrix = new string[,] { { "ha", "fifi", "ho", "hi" }, { "fo", "ha", "hi", "xx" }, { "xxx", "ho", "ha", "xx" } };
+
             String input;
 
-            Console.WriteLine("Enter strings elements");
-            for (int row = 0; row < rows; row++)
-            {
-                for (int column = 0; column < columns; column++)
-                {
-                    Console.Write("matrix[{0},{1}] = ", row, column);
-                    input = Console.ReadLine();
-                    matrix[row, column] = input;
-                }
-            }
+            // Console.WriteLine("Enter strings elements");
+            // for (int row = 0; row < rows; row++)
+            // {
+            //     for (int column = 0; column < columns; column++)
+            //     {
+            //         Console.Write("matrix[{0},{1}] = ", row, column);
+            //         input = Console.ReadLine();
+            //         matrix[row, column] = input;
+            //     }
+            // }
+
+
 
             Print(matrix);
         }
@@ -48,7 +52,7 @@ namespace Namespace
             {
                 for (int column = 0; column < data.GetLength(1); column++)
                 {
-                    Console.Write("[{0,{1}}] ", data[row, column],6);
+                    Console.Write("[{0,4}] ", data[row, column]);
                 }
                 Console.WriteLine();
             }
